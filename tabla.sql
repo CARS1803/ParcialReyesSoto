532ee06b13c0ff50b32ac77b444bb68c329747d6
Reyes <Carlosreyes1803@gmail.com>
2023-05-22 09:37:00 -0600

    Creacion de archivos y carpetas

---
+++ tabla.sql
@@ -0,0 +1,7 @@
+CREATE TABLE cursos(
+    nombre_curso CHAR(50) not null,
+    cantidad_periodos INT not null,
+    modulo_impartido INT not null,
+    nombre_instructor CHAR(50) not null,
+    creditos_curso DECIMAL not null
+);
