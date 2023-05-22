98a2da33e0731d9cff481d47da2cacd0e0129099
Reyes <Carlosreyes1803@gmail.com>
2023-05-22 09:54:56 -0600

    Doc. para establecer la conexion a la bd.

---
+++ modelos/Conexion.php
@@ -0,0 +1,22 @@
+<?php
+//Doc. para establecer la conexion a la bd.
+abstract class Conexion{
+    public static $conexion = null;
+//Creamos la funcion conectar para conectarnos.
+    private static function conectar(){
+        try{
+            //Conectamos la base de datos de informix localmente. 
+            self::$conexion = new PDO('informix:host=host.docker.internal; service=9088; database=mdn; server=informix; protocol=onsoctcp;EnableScrollableCursors = 1','informix','in4mix'); 
+            self::$conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }catch(PDOException $e){
+            // Imprimir error si existen complicaiones.
+            echo "Error de conexion de BD";
+            echo "<br>";
+            echo $e->getMessage();
+            exit;
+        }
+        //regresamos la conexion.
+        return self::$conexion;
+    }
+
+}
